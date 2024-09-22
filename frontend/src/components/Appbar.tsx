b848f65f240c26ca753e5735d1a009daedc173ea
Dhruv <dhruvsrivastava11@gmail.com>
2024-09-22 15:50:20 +0530

    padding

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import { Avatar } from "./BlogCard"
 
 export const Appbar = () => {
-    return <div className="border-b flex justify-between px-10 py-4">
+    return <div className="border-b flex justify-between px-6 py-4">
         <Link to={`/blogs`} className="font-extrabold cursor-pointer flex flex-col justify-center">
                 Medium
         </Link>
